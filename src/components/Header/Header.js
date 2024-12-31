b98a560e8e1fdf2a92a3f40de9e208bf22570c7e
Madam Vignesh <madamvignesh@gmail.com>
2024-12-31 18:31:44 +0530

    mobile updated v2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header className="header-container">
       <div className="header-logo">
         <h1>Transaction Management</h1>
       </div>
-      <nav className="header-nav">
+      <div 
+        className={`menu-card ${menuActive ? "active" : ""}`} 
+        onClick={toggleMenu}
+      >
+        Menu
+      </div>
+      <nav className={`header-nav ${menuActive ? "active" : ""}`}>
         <ul className="nav-links">
-            <li>
-                <NavLink to="/" className="nav-link" activeClassName='active-link'>Home</NavLink>
-            </li>
           <li>
-            <NavLink 
-              to="/api/transactions" 
-              className="nav-link" 
-              activeClassName="active-link">
+            <NavLink to="/" className="nav-link" activeClassName="active-link">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/api/transactions"
+              className="nav-link"
+              activeClassName="active-link"
+            >
               Create Transaction
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/api/view-transactions" 
-              className="nav-link" 
-              activeClassName="active-link">
+            <NavLink
+              to="/api/view-transactions"
+              className="nav-link"
+              activeClassName="active-link"
+            >
               View Transactions
             </NavLink>
           </li>
